c48a0f8d6f4deb2ee49bb8e4c3f4504ae2736663
someonecoding <54615836+someonecoding@users.noreply.github.com>
2022-07-15 21:50:03 +0300

    added MinLengthValidator and modified AbstractBaseValidator

--- py_task2.py
+++ py_task2.py
@@ -3,16 +3,36 @@ from abc import ABC, abstractclassmethod
 
 
 class AbstractBaseValidator(ABC):
+
+    value_type = str
     
     class ValidationError(Exception):
         def __init__(self, message):
             self.message = message
 
+    class ValueTypeError(Exception):
+        def __init__(self, message):
+            self.message = message
+
+    @abstractclassmethod
+    def validate_type(cls, value):
+        if not isinstance(value, cls.value_type):
+            raise cls.ValueTypeError(f'Value type must be {cls.value_type}.')
+
     @abstractclassmethod
     def validate(cls, value):
-        pass
+        cls.validate_type(value)
 
 
+class MinLengthValidator(AbstractBaseValidator):
+
+    min_length = 4
+
+    @classmethod
+    def validate(cls, value):
+        super().validate(value)
+        if not len(value) >= cls.min_length:
+            raise cls.ValidationError(f'Length must be at least {cls.min_length}')
 
 
 
